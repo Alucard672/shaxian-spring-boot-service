5bdc0b38ac3999d2c8ac34193e419df99a435dca
qiuxs <qiuxs@qiuxs.com>
2026-01-28 18:17:38 +0800

    去除无效操作

--- scripts/push_codes.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rsync -av --progress --exclude-from=.gitignore ./ root@120.27.148.45:/data/codes/shaxian-spring-boot-service/
--- scripts/run_dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "正在设置 Java 17 环境..."
-export JAVA_HOME=$(/usr/libexec/java_home -v 17)
-
-echo "正在启动应用..."
-mvn clean -DskipTests spring-boot:run --spring.profiles.active=qiuxs
